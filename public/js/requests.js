1ce4949773530537e97273df4b5d3cca35fcf3a6
Aleksandr Jevlentjev <aleksandr@LT-LIT-SC-0010.internal.visma.com>
2020-02-14 12:08:22 +0200

    xhr modified

--- public/js/requests.js
+++ public/js/requests.js
@@ -4,26 +4,29 @@ const backUrl = 'http://localhost:3000';
 function sendRequest(method,url,data,callback) {
 
     let xhr = new XMLHttpRequest();
-    xhr.open(method,url);
+    xhr.open(method,url,true);
     xhr.setRequestHeader('Content-type', 'application/json');
-    xhr.send(JSON.stringify(data));
     
     xhr.onerror = function() {
         console.log('Request failed');
     };
 
     xhr.onload = function() {
-        if (xhr.status === 200) { 
-            let response = JSON.parse(xhr.response);
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) { 
+                let response = JSON.parse(xhr.response);
 
-            callback(response);   
-            
-        } else {
+                callback(response);   
+                
+            } else {
 
-            console.log('Error status: ' + xhr.status + '. ' + xhr.response);                
+                console.log('Error status: ' + xhr.status + '. ' + xhr.response);                
 
+            }
         }
     }
+
+    xhr.send(JSON.stringify(data));
 }
 
 
