f03204d819adb966d5d3d7a0330124b023d4c886
Aleksandr Jevlentjev <aleksandr@LT-LIT-SC-0010.internal.visma.com>
2020-02-18 16:46:39 +0200

    js code cleaned

--- public/js/admin.js
+++ public/js/admin.js
@@ -5,38 +5,32 @@ const image = document.querySelector('#image');
 const discount = document.querySelector('#discount');
 const checkbox = document.querySelector('#checkbox');
 const status_el = document.querySelector('#status');
-
 let deleteButton, isEditMode;
 
 itemId = window.location.search.slice(4);
 isEditMode = !!itemId;
-
-isEditMode && sendRequest('POST', backUrl + '/items', [itemId] ,prepareEdit);
-
+isEditMode && sendRequest('GET', '/items?id=' + itemId, null, prepareEdit);
 
 function prepareEdit(res) {
     name.value = res.name;
     price.value = res.price;
     image.value = res.image;
-
     if (res.discount) {
         checkbox.checked = res.discount;
         discount.value = res.discount;
     } 
-
-    deleteButtonCreate();
+    createDeleteButton();
 }
 
-function deleteButtonCreate() {
+function createDeleteButton() {
     const button = document.createElement('button');
     button.classList.add('form__delete');
     button.setAttribute('type','button');
     button.textContent = 'Delete';
-    form.appendChild(button);
-
     button.onclick = function() {
-        sendRequest('DELETE', backUrl + '/items/delete', [itemId] ,successCallback);
+        sendRequest('DELETE', '/items?id=' + itemId, null, successCallback);
     }
+    form.appendChild(button);
 }
 
 function successCallback() {
@@ -52,16 +46,13 @@ function successCallback() {
 
 function formHandle(e) {
     e.preventDefault();
-
     const data = {
         name: name.value,
         price: price.value,
         image: image.value,
         discount: (checkbox.checked ? discount.value : 0) 
     };
-
-    isEditMode && sendRequest('PUT', backUrl + '/items/update',[itemId,data],successCallback); 
-    !isEditMode && sendRequest('POST', backUrl + '/items/add',data,successCallback);
-    
+    isEditMode && sendRequest('PUT', '/items?id=' + itemId, data, successCallback); 
+    !isEditMode && sendRequest('POST', '/items', data, successCallback);
     return false;
 }
